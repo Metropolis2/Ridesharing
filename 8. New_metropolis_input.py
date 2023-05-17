2f5013b8ded4a04d8b57a0e3ad47a7f6b56b7995
Samarth Ghoslya <102518260+Samghoslya@users.noreply.github.com>
2023-05-17 16:19:12 +0200

    Update 8. New_metropolis_input.py

--- 8. New_metropolis_input.py
+++ 8. New_metropolis_input.py
@@ -16,17 +16,19 @@ df1 = df['id passenger'].tolist()
 #Agents input read
 i_agents = json.load(open('agents.json'))
 input_01 = []
-p = 0
+
 for x in i_agents:
     if x['id'] in df1:
-        print(x['id'])
+        continue
     else:
         id = {"id": x["id"]}    
         my_list = x
         input_01.append(my_list)
-    p += 1
+
 
 json_string = json.dumps(input_01)
 # Write the JSON formatted string to a file
 with open('new_metropolis_input.json', 'w') as file:
     file.write(json_string)
+
+print("% s seconds" % (time.time() - start))
